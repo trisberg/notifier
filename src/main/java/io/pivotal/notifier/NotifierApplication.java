8d5f3f55f949eae30a4f8bb6c5d3708290efca25
Dodd Pfeffer <dpfeffer@pivotal.io>
2018-10-24 23:52:34 -0400

    Adds scripts and handles empty message

--- src/main/java/io/pivotal/notifier/NotifierApplication.java
+++ src/main/java/io/pivotal/notifier/NotifierApplication.java
@@ -1,11 +1,13 @@
 package io.pivotal.notifier;
 
+import com.google.gson.Gson;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.util.StringUtils;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -23,12 +25,22 @@ public class NotifierApplication implements Consumer<Message> {
 
 	@Override
 	public void accept(Message message) {
-		SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-		simpleMailMessage.setTo(message.getTo());
-		simpleMailMessage.setSubject(message.getSubject());
-		simpleMailMessage.setText(message.getBody());
-		simpleMailMessage.setFrom(message.getFrom());
-		emailSender.send(simpleMailMessage);
-		log.info("Sent email to " + message.getTo());
+
+		Gson gson = new Gson();
+		String json = gson.toJson(message);
+
+		log.info("Received: " + json);
+
+		if(!StringUtils.isEmpty(message.getBody())) {
+			SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+			simpleMailMessage.setTo(message.getTo());
+			simpleMailMessage.setSubject(message.getSubject());
+			simpleMailMessage.setText(message.getBody());
+			simpleMailMessage.setFrom(message.getFrom());
+			emailSender.send(simpleMailMessage);
+			log.info("Sent email to " + message.getTo());
+		} else {
+			log.info("Suppressing email, no body.");
+		}
 	}
 }
